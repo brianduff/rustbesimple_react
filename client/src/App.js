6838c57a43bae4e577a9e1d65ef292a1e26e4864
Brian Duff <cairndubh@gmail.com>
2021-03-26 18:32:54 -0700

    Call api

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 export default function App() {
-  return <div className="App-Div">Hello There!</div>;
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      // TODO: this should not be hardcoded, and should vary between
+      // dev & production.
+      const response = await axios("http://localhost:8000/api/name")
+      setName(response.data)
+    }
+    fetchData()
+  }, []);
+
+  return <div className="App-Div">Hello {name}</div>;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
 #![feature(proc_macro_hygiene, decl_macro, backtrace)]
 
-use rocket::{get, routes};
+use rocket::{get, routes, Request, Response};
 use rocket_contrib::serve::StaticFiles;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
 
-#[get("/hello")]
-fn get_hello() -> String {
-  "Hello World".to_owned()
+#[get("/name")]
+fn get_name() -> String {
+  "Brian Duff".to_owned()
+}
+
+// CORS fairing so we can access the api from a different port on localhost.
+// This should be disabled in production.
+
+pub struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response
+        }
+    }
+
+    fn on_response(&self, _: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
 }
 
 fn main() {
   rocket::ignite()
-    .mount("/api/", routes![get_hello])
+    .attach(CORS())
+    .mount("/api/", routes![get_name])
     .mount("/", StaticFiles::from("client/build"))
     .launch();
 }
